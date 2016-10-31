065e003bd48e83c7af6aa60049f488ea99a9876a
grinnelldennis <chanchun@grinnell.edu>
2016-10-30 22:51:52 -0500

    Updated JavaDoc

--- src/edu/grinnell/sortingvisualizer/sortevents/SortEvent.java
+++ src/edu/grinnell/sortingvisualizer/sortevents/SortEvent.java
@@ -2,10 +2,25 @@ package edu.grinnell.sortingvisualizer.sortevents;
 
 import java.util.List;
 
+/**
+ * @author Mattori Birnbaum & Dennis Chan
+ *	SortEvent Interface, 
+ *	Creates generic sortevent class 
+ */
 public interface SortEvent<T> {
+	/**
+	* void apply(arr): applies this sort event to the given array.
+	*/
 	void apply(T[] arr);
 	
+	/**
+	* List<Integer> getAffectedIndices(): returns a list containing all of the array 
+	*  indices that this event affects.
+	*/
 	List<Integer> getAffectedIndices();
 	
+	/**
+	* return true if this event should be emphasized by the visualizer/audibilizer.
+	*/
 	boolean isEmphasized();
 }
